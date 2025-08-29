445c06a55dc225840c797a627e178f7db093fe3f
jcrumb-TTU <jcrumb42@tntech.edu>
2025-08-28 20:54:14 -0500

    Created and populated users.cs with initial variables

---
+++ Hippo-Exchange/users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class users
+{
+	public users()
+	{
+		public string? strUserID { get; set; }
+		public string? strFirstName { get; set; }
+		public string? strLastName { get; set; }
+		public string? strEmail { get; set; }
+		public string? strPasswordHash { get; set; }
+		public DateTime dtCreated { get; set; }
+
+	}
+}
+
