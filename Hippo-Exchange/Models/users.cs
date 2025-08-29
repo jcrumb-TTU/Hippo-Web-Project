c983feac4b30ecda38d80080dffe20b9c29f37b6
jcrumb-TTU <jcrumb42@tntech.edu>
2025-08-29 17:29:52 -0500

    Edited and finalized user class prototype with password hashing

--- Hippo-Exchange/Models/users.cs
+++ Hippo-Exchange/Models/users.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using Isopoh.Cryptography.Argon2;
+using Microsoft.AspNetCore.Identity;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 
-public class users
+namespace Hippo_Exchange.Models
 {
-	public users()
+	[BsonIgnoreExtraElements]
+    public class Users
 	{
-		public string? strUserID { get; set; }
-		public string? strFirstName { get; set; }
-		public string? strLastName { get; set; }
-		public string? strEmail { get; set; }
-		public string? strPasswordHash { get; set; }
-		public DateTime dtCreated { get; set; }
+		[BsonId]
+        public string? strUserID { get; set; }
+		[BsonElement("FirstName")]
+        public string? strFirstName { get; set; }
+		[BsonElement("LastName")]
+        public string? strLastName { get; set; }
+		[BsonElement("Email")]
+        public string? strEmail { get; set; }
+		[BsonElement("phoneNumber")]
+        public string? strPhoneNumber { get; set; }
+		[BsonElement("PasswordHash")]
+        public string? strPasswordHash { get; set; }
 
-	}
+        public void SetPassword(string strPassword)
+        {
+            if (string.IsNullOrWhiteSpace(strPassword))
+                throw new ArgumentException("Password must not be empty.", nameof(password));
+
+            strPasswordHash = Argon2.Hash(strPassword);
+        }
+
+    }
 }
 
